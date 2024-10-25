7799007a2fece63f615c29082ff57ea9809a4547
amithsoddy <anithasoddy@gmail.com>
2024-10-25 20:52:16 +0530

    armstrong number

---
+++ armstrong.py
@@ -0,0 +1,10 @@
+x=int(input("enter a number"))
+if x>1:
+    for i in range(2,int(1^3+5^3^3)+1):
+        if x%i==0:
+            print(f"{x}is a armstrong number")
+            break
+        else:
+            print(f"{x}is a armstrong number")
+else:
+     print(f"{x}is not a armstrong number")
